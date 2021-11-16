5a34f98b194c7a0314af5489b50bcab67168c77b
nishantshah <nishantshah1729@gmail.com>
2021-11-16 13:54:56 +0530

    Node modules added

--- utils.js
+++ utils.js
@@ -1,3 +1,7 @@
 const os = require('os')
-console.log(os.userInfo()); 
-console.log(os.uptime()); 
+const path = require('path')
+const fs = require('fs')
+// console.log(os.userInfo()); 
+// console.log(os.uptime()); 
+console.log(path.sep);
+console.log(fs);
