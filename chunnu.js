28f7284583bb2c51764d1373346aa563ace43f7f
nishantshah <nishantshah1729@gmail.com>
2021-11-18 14:23:15 +0530

    lint added

--- chunnu.js
+++ chunnu.js
@@ -1,19 +1,43 @@
-const { resolve } = require("path/posix");
-
 let stocks = {
     Fruits: ["strawberry", "grapes", "banana", "apple"],
     liquid: ["water", "ice"],
     holder: ["cone", "cup", "stick"],
     toppings: ["chocolate", "peanuts"]
 }
-let is_store_open = true;
-let order = (time, work) => {
-    return new Promise((resolve, reject) => {
-        if (is_store_open) {
-            setTimeout(() => { resolve(work()) }, time)
+// let is_store_open = 1;
+// const order = (time, work) => {
+//     return new Promise((resolve, reject) => {
+//         if (is_store_open) {
+//             setTimeout(() => {
+//                 resolve(work())
+//             }, time)
 
-        } else {
-            reject(console.log("Our Shop is Closed!"))
-        }
-    })
+//         } else {
+//             reject(console.log(`Our Shop is Closed!`))
+//         }
+//     })
+// }
+// order(2000, () => console.log(`${stocks.Fruits[0]} was selected`))
+
+//     .then(() => order(1000, () => console.log(`Production has started`)))
+//     .then(() => order(2000, () => console.log(`The Fruit was chopped!`)))
+//     .then(() => order(1000, () => console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was selected`))
+//         .then(() => order(1000, () => console.log(`Start the machine`)))
+//         .then(() => order(2000, () => console.log(`Ice-cream was placed on ${stocks.holder[0]}`)))
+//         .then(() => order(3000, () => console.log(`${stocks.toppings[0]} was selected`)))
+//         .then(() => order(1000, () => console.log(`Ice-cream was served`)))
+//         .catch(() => { })
+//         .finally(() => console.log(`Sayonara!!!`)
+
+
+async function order() {
+    try {
+
+    }
+    catch(error) {
+
+    }
+    finally {
+
+    }
 }
