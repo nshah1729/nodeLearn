c0742764af8af63cf98435c135903d087c35329e
nishantshah <nishantshah1729@gmail.com>
2021-11-19 13:51:33 +0530

    f

--- chunnu.js
+++ chunnu.js
@@ -1,17 +1,27 @@
-const fs = require('fs').promises;
+// const fs = require('fs').promises;
 
-const start = async () => {
-    try {
-        const first = await fs.readFile('./content/first.txt', 'utf-8');
-        const second = await fs.readFile('./content/second.txt', 'utf-8');
-        await fs.writeFile('./content/third.txt', 'utf');
+// const start = async () => {
+//     try {
+//         const first = await fs.readFile('./content/first.txt', 'utf-8');
+//         const second = await fs.readFile('./content/second.txt', 'utf-8');
+//         await fs.writeFile('./content/third.txt', 'utf');
 
-        console.log(first);
-        console.log(second);
+//         console.log(first);
+//         console.log(second);
 
-    } catch (error) {
-        console.log(error);
-    }
-}
+//     } catch (error) {
+//         console.log(error);
+//     }
+//     finally{
+//         console.log("DOne");
+//     }
+// }
 
-start()
+// start()
+
+const EventEmitter= require('events')
+const customEmitter=new EventEmitter();
+customEmitter.on('response',()=>{
+    console.log(`Data Recieved!!!`);
+})
+customEmitter.emit('response')
