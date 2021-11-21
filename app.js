46fd7bea064f7da6f02c49f3bfbd30497b0abfa0
nishantshah <nishantshah1729@gmail.com>
2021-11-21 09:55:26 +0530

    f

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
+const http = require('http');
 const fs = require('fs');
-
-const getText = (path) => new Promise((resolve, reject) => {
-  fs.readFile(path, 'utf-8', (err, data) => {
-    if (err) {
-      // eslint-disable-next-line prefer-promise-reject-errors
-      reject('ee');
-    } else {
-      resolve(data);
-    }
-  });
-});
-getText('./content/first.txt')
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err))
-  .finally(() => console.log('Finished!'));
+const homePage = fs.readFileSync('./navbar-app/index.html')
+const homeStyles = fs.readFileSync('./navbar-app/styles.css')
+const homeImage = fs.readFileSync('./navbar-app/logo.svg')
+const homeLogic = fs.readFileSync('navbar-app/browser-app.js')
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === '/') {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    res.write(homePage)
+    console.log(url);
+    res.end()
+  }
+  else if (url === '/styles.css') {
+    res.writeHead(200, { 'content-type': 'text/css' })
+    res.write(homeStyles)
+    res.end()
+  }
+  else if (url === '/browser-app.js') {
+    res.writeHead(200, { 'content-type': 'text/javascript' })
+    res.write(homeLogic)
+    res.end()
+  }
+  else if (url === '/logo.svg') {
+    res.writeHead(200, { 'content-type': 'image/svg+xml' })
+    res.write(homeImage)
+    res.end()
+  }
+})
+server.
+  listen(3000, () => console.log(`Server Started`));
