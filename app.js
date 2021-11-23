66235e40b4fc664679eff5c63dcc658c3bf6ac81
nishantshah <nishantshah1729@gmail.com>
2021-11-23 20:15:35 +0530

    bfskj

--- app.js
+++ app.js
@@ -33,3 +33,13 @@
 //   listen(3000, () => console.log(`Server Started`));
 
 const express = require('express');
+const path = require('path');
+const app = express();
+
+//Setup static and Middleware...static are files that the server does not have to change!
+app.use(express.static('./public'))
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+})
+app.all('*', (req, res) => res.status(404).send(`Error 404...Page not Found!`))
+app.listen('3000', () => console.log(`The server has started at Port 3000...`))
