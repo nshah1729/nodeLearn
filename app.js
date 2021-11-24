b0c02eadf705b4bd56ac838aeadd6dc540f39139
nishantshah <nishantshah1729@gmail.com>
2021-11-24 13:29:55 +0530

    query

--- app.js
+++ app.js
@@ -33,17 +33,36 @@
 //   listen(3000, () => console.log(`Server Started`));
 
 const express = require('express');
-const path = require('path');
+const { products } = require('./data');
 const app = express();
 
-//Setup static and Middleware...static are files that the server does not have to change!
-app.use(express.static('./public'))
-
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+    res.send('<h1>Home Page</h1><a href="api/products">Products</a>');
+})
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const { id, name, image } = product
+        return { id, name, image };
+    })
+    res.json(newProducts)
+})
+app.get('/api/v1/query', (req, res) => {
+    const { search, limit } = req.query;;
+    let sortedProducts = [...products]
+    if (search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search)
+        })
+    }
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    if (sortedProducts.length < 1) {
+        return res.status(200).send('The product was not found!')
+    }
+    return res.status(200).json(sortedProducts)
 })
 
-app.all('*', (req, res) => res.status(404).send(`Error 404...Page not Found!`))
-
-
-app.listen('3000', () => console.log(`The server has started at Port 3000...`))
+app.listen(3000, () => {
+    console.log(`The Server is listening`);
+})
