42858ff09241e7b97a97b138260bccfbe977b036
nishantshah <nishantshah1729@gmail.com>
2021-12-01 10:21:53 +0530

    fse

--- kc.js
+++ kc.js
@@ -1,4 +1,15 @@
 // console.log( [2,4,6,8].filter((e)=>e>7));
-const a=[1,34,5];
-a[0]=444;
-console.log(a);
+// const a=[1,34,5];
+// a[0]=444;
+// console.log(a);
+let user={
+    name:"Nishant Shah",
+    Roll: "001911102028",
+    Dept:"IEE",
+    changeCourse:function (){
+        console.log(this);
+    }
+}
+// console.table(user);
+user.changeCourse();
+// console.table(user);
